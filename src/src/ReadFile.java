56c5d14b25b123c7572159a7c8d7b1a3e67c855c
Matt Neal <mattneal@iupui.edu>
2017-04-09 15:11:51 -0400

    begin working on readfile

--- src/src/ReadFile.java
+++ src/src/ReadFile.java
@@ -1,5 +1,33 @@
+import java.io.IOException;
+import java.io.FileReader;
+
 /**
- * Created by matt on 4/9/2017.
+ * File input class that reads in desired data to be processed by either
+ * an encrypter or decrypter. Requires a file name as an input string.
  */
 public class ReadFile {
+    String path;
+
+    public void readFileIn(String filePath) throws IOException {
+        FileReader inputStream = null;
+
+        try {
+            inputStream = new FileReader("xanadu.txt");
+            int c;
+
+            while ((c = inputStream.read()) != -1) {
+                outputStream.write(c);
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    }
+}
+    }
+
 }
