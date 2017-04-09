5dc5969377b3646a95b751a45beee727f25ab898
Matt Neal <mattneal@iupui.edu>
2017-04-09 18:17:44 -0400

    Add IOException where required.

--- src/src/CommandFactory.java
+++ src/src/CommandFactory.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 /**
  * Command factory which creates new instances of each potential decryption (potentially
  * encryption for future) and calls that method's required methods so as to abstract out
@@ -5,22 +7,22 @@
  */
 class CommandFactory {
 
-    void anagram(){
+    void anagram() throws IOException {
         Anagram a = new Anagram();
         a.execute();
     }
 
-    void keyword(){
+    void keyword() throws IOException {
         Keyword k = new Keyword();
         k.execute();
     }
 
-    void shift(){
+    void shift() throws IOException {
         Shift s = new Shift();
         s.execute();
     }
 
-    void automate(){
+    void automate() throws IOException {
         Automate au = new Automate();
         au.execute();
     }
