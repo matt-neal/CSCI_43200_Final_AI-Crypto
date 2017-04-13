42239b2fcec2a9368efb992e94242cae2ea7e3e7
Matt Neal <mattneal@iupui.edu>
2017-04-13 16:16:41 -0400

    Clean up Shift. Create a controller for shift. Implement loop functionality so that the user determines when the shift is complete and then save to a file.

--- Crypto/src/AnagramHash.java
+++ Crypto/src/AnagramHash.java
@@ -1,72 +1,73 @@
-/** Anagram
- *  Permutates the strings of characters creating different words
- *  words are searched  within the dictionary.
- *  If the dictionary contains the word, it will be printed.
- */
-
-import java.awt.*;
-import java.io.*;
-import java.util.*;
-
-class AnagramHash {
-    ArrayList<String> possibleWords = new ArrayList<String>();
-    Dictionary dict;
-
-    // Recursively find permutations of given string of characters.
-    private ArrayList<String> permutations(String word) {
-        ArrayList<String> list = new ArrayList<String>();
-
-        if (word.length() == 1) {
-            list.add(word);
-            return list;
-        }
-
-        for (int i=0; i<word.length(); i++) {
-            String shorter = word.substring(0, i) + word.substring(i+1);
-
-            ArrayList<String> sublist = permutations(shorter);
-
-            for (String s : sublist) {
-                list.add(word.charAt(i) + s);
-            }
-        }
-
-        return list;
-    }
-
-    public AnagramSolver(String text, String filePath) throws FileNotFoundException {
-            System.out.println("Using hash dictionary.");
-            dict = new HashDictionary(new File(filePath));
-
-        for (String each : permutations(text)) {
-            possibleWords.add(each);
-        }
-    }
-
-    public String solve() {
-        for (String word : possibleWords) {
-            System.out.println("Searching for: " + word);
-            if (dict.contains(word)) {
-                //System.out.println(dict.getAverageLookupTime());
-                return word;
-            }
-        }
-        return null;
-    }
-
-    public static void execute (String[] args) throws FileNotFoundException {
-        ReadFile RF = new ReadFile();
-        WriteFile WF = new WriteFile();
-
-        // Read input text using defined method
-        String text = RF.readBlockIn(Charset.defaultCharset());
-
-        AnagramSolver as = new AnagramSolver(args[0], args[1]);//(String anagram, String pathToFile)
-        for (int i=0; i<10; i++) {
-            System.out.println("Anagram found: " + as.solve());
-        }
-
-        WF.outputFile(as.solve());
-        System.out.println(as.solve());
-    }
-}
+///** Anagram
+// *  Permutates the strings of characters creating different words
+// *  words are searched  within the dictionary.
+// *  If the dictionary contains the word, it will be printed.
+// */
+//
+//import java.awt.*;
+//import java.io.*;
+//import java.nio.charset.Charset;
+//import java.util.*;
+//
+//class AnagramHash {
+//    ArrayList<String> possibleWords = new ArrayList<String>();
+//    Dictionary dict;
+//
+//    // Recursively find permutations of given string of characters.
+//    private ArrayList<String> permutations(String word) {
+//        ArrayList<String> list = new ArrayList<String>();
+//
+//        if (word.length() == 1) {
+//            list.add(word);
+//            return list;
+//        }
+//
+//        for (int i=0; i<word.length(); i++) {
+//            String shorter = word.substring(0, i) + word.substring(i+1);
+//
+//            ArrayList<String> sublist = permutations(shorter);
+//
+//            for (String s : sublist) {
+//                list.add(word.charAt(i) + s);
+//            }
+//        }
+//
+//        return list;
+//    }
+//
+//    public void AnagramSolver(String text, String filePath) throws FileNotFoundException {
+//            System.out.println("Using hash dictionary.");
+//            dict = new HashDictionary(new File(filePath));
+//
+//        for (String each : permutations(text)) {
+//            possibleWords.add(each);
+//        }
+//    }
+//
+//    public String solve() {
+//        for (String word : possibleWords) {
+//            System.out.println("Searching for: " + word);
+//            if (dict.contains(word)) {
+//                //System.out.println(dict.getAverageLookupTime());
+//                return word;
+//            }
+//        }
+//        return null;
+//    }
+//
+//    public static void execute (String[] args) throws IOException {
+//        ReadFile RF = new ReadFile();
+//        WriteFile WF = new WriteFile();
+//
+//        // Read input text using defined method
+//        String text = RF.readBlockIn(Charset.defaultCharset());
+//
+//        AnagramSolver(args[0], args[1]);//(String anagram, String pathToFile)
+//        for (int i=0; i<10; i++) {
+//            System.out.println("Anagram found: " + solve());
+//        }
+//
+//        WF.outputFile(solve());
+//        System.out.println(solve());
+//    }
+//}
--- Crypto/src/Transposition.java
+++ Crypto/src/Transposition.java
@@ -1,80 +1,80 @@
-//BRUTE FORCE TRANSPOSITION USING
-//4 COLUMN KEY AND RANDOMLY GENERATES
-//PERMUTATION OF 1-4 TO CHANGE COLUMN ORDER
-//SHOULD CHECK AGAINST DICTIONARY FOR CORRECT PERMUTATION ORDER
-
-//CAN ALSO BE DONE DYNAMICALLY (EXHAUSTIVELY USING DICTIONARY ATTACK)
-//BY GUESSING AT KEYWORDS IN THE DICTIONARY TEXT FILE
-
-import java.util.*;
-import java.io.*;
-import java.lang.*;
-
-class Transposition {
-    static String text = new readFile().readFileContents();
-
-    //ARRANGE STRING IN GRID FORMAT
-    public static int[] arrangeKey(String key) {
-        //arranges position of grid
-        //i.e. if key is 4 letter word, grid is four columns wide
-        //String[] keys = key.split(""); //if using dynamically changing key
-        String[] keys = 4;
-        Arrays.sort(keys);
-        int[] num = new int[key.length()];
-        for (int x = 0; x < keys.length; x++) {
-            for (int y = 0; y < key.length(); y++) {
-                if (keys[x].equals(key.charAt(y) + "")) {
-                    num[y] = x;
-                    break;
-                }
-            }
-        }
-
-        return num;
-    }
-
-    //reorders columns according to key
-    public static String decrypt(String key, String text) {
-        //key = random permutations of 4
-        int[] arrange = arrangeKey(key);
-        int lenkey = arrange.length;
-        int lentext = text.length();
-
-        int row = (int) Math.ceil((double) lentext / lenkey);
-
-        String regex = "(?<=\\G.{" + row + "})";
-        String[] get = text.split(regex);
-
-        char[][] grid = new char[row][lenkey];
-
-        for (int x = 0; x < lenkey; x++) {
-            for (int y = 0; y < lenkey; y++) {
-                if (arrange[x] == y) {
-                    for (int z = 0; z < row; z++) {
-                        grid[z][y] = get[arrange[y]].charAt(z);
-
-                        //can check against dictionary
-                        //or attempt entropy (frequency amount) from shift
-                        //that way we don't have to reconcatenate strings
-                    }
-                }
-            }
-        }
-        String dec = "";
-        for (int x = 0; x < row; x++) {
-            for (int y = 0; y < lenkey; y++) {
-                dec = dec + grid[x][y];
-            }
-        }
-
-        return dec;
-    }
-
-    public static void execute(String[] args) {
-        String line = System.getProperty("line.separator");
-        scan.useDelimiter(line);
-
-        System.out.println(decrypt(key, text));
-        break;
-    }
-}
+////BRUTE FORCE TRANSPOSITION USING
+////4 COLUMN KEY AND RANDOMLY GENERATES
+////PERMUTATION OF 1-4 TO CHANGE COLUMN ORDER
+////SHOULD CHECK AGAINST DICTIONARY FOR CORRECT PERMUTATION ORDER
+//
+////CAN ALSO BE DONE DYNAMICALLY (EXHAUSTIVELY USING DICTIONARY ATTACK)
+////BY GUESSING AT KEYWORDS IN THE DICTIONARY TEXT FILE
+//
+//import java.util.*;
+//import java.io.*;
+//import java.lang.*;
+//
+//class Transposition {
+//    static String text = new readFile().readFileContents();
+//
+//    //ARRANGE STRING IN GRID FORMAT
+//    public static int[] arrangeKey(String key) {
+//        //arranges position of grid
+//        //i.e. if key is 4 letter word, grid is four columns wide
+//        //String[] keys = key.split(""); //if using dynamically changing key
+//        String[] keys = 4;
+//        Arrays.sort(keys);
+//        int[] num = new int[key.length()];
+//        for (int x = 0; x < keys.length; x++) {
+//            for (int y = 0; y < key.length(); y++) {
+//                if (keys[x].equals(key.charAt(y) + "")) {
+//                    num[y] = x;
+//                    break;
+//                }
+//            }
+//        }
+//
+//        return num;
+//    }
+//
+//    //reorders columns according to key
+//    public static String decrypt(String key, String text) {
+//        //key = random permutations of 4
+//        int[] arrange = arrangeKey(key);
+//        int lenkey = arrange.length;
+//        int lentext = text.length();
+//
+//        int row = (int) Math.ceil((double) lentext / lenkey);
+//
+//        String regex = "(?<=\\G.{" + row + "})";
+//        String[] get = text.split(regex);
+//
+//        char[][] grid = new char[row][lenkey];
+//
+//        for (int x = 0; x < lenkey; x++) {
+//            for (int y = 0; y < lenkey; y++) {
+//                if (arrange[x] == y) {
+//                    for (int z = 0; z < row; z++) {
+//                        grid[z][y] = get[arrange[y]].charAt(z);
+//
+//                        //can check against dictionary
+//                        //or attempt entropy (frequency amount) from shift
+//                        //that way we don't have to reconcatenate strings
+//                    }
+//                }
+//            }
+//        }
+//        String dec = "";
+//        for (int x = 0; x < row; x++) {
+//            for (int y = 0; y < lenkey; y++) {
+//                dec = dec + grid[x][y];
+//            }
+//        }
+//
+//        return dec;
+//    }
+//
+//    public static void execute(String[] args) {
+//        String line = System.getProperty("line.separator");
+//        scan.useDelimiter(line);
+//
+//        System.out.println(decrypt(key, text));
+//        break;
+//    }
+//}
